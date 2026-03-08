33ef5c2072289aed7bf7d8253fff5441b4c9566f
Rokibul Islam Robi <149813484+Rokibul-Islam-Robi@users.noreply.github.com>
2026-03-09 03:28:47 +0600

    fix: split manager files and install missing type declarations for howler

--- frontend/src/managers/AudioManager.ts
+++ frontend/src/managers/AudioManager.ts
@@ -1,1 +1,18 @@
-export class AudioManager {}
+// frontend/src/managers/AudioManager.ts
+import { Howl } from 'howler';
+
+export class AudioManager {
+    sounds: { [key: string]: Howl } = {};
+
+    constructor() {
+        this.sounds = {
+            click: new Howl({ src: ['https://assets.mixkit.co/sfx/preview/mixkit-simple-click-630.mp3'] }),
+            success: new Howl({ src: ['https://assets.mixkit.co/sfx/preview/mixkit-winning-chimes-2015.mp3'] }),
+            bonus: new Howl({ src: ['https://assets.mixkit.co/sfx/preview/mixkit-shiny-objects-2016.mp3'] })
+        };
+    }
+
+    playSound(name: string) {
+        if (this.sounds[name]) this.sounds[name].play();
+    }
+}
